b8991721fa13eef861b87ad9719b10da0d3a3634
gsick <gamaliel.sick@yahoo.fr>
2014-06-21 16:36:13 +0200

    initial commit

---
+++ spec/classes/openresty_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe 'openresty' do
+
+  let(:hiera_config) { 'spec/fixtures/hiera/hiera.yaml' }
+  let(:parser) { 'future' }
+
+  it { should contain_package('wget') }
+  it { should contain_package('perl') }
+  
+end
