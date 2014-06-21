0946806242f7809348fc4ddb2c4376e672ca025b
gsick <gamaliel.sick@yahoo.fr>
2014-06-21 19:22:16 +0200

    initial commit

--- spec/classes/openresty_spec.rb
+++ spec/classes/openresty_spec.rb
@@ -12,7 +12,7 @@ describe 'openresty' do
   it { should contain_package('pcre-devel') }
   it { should contain_package('openssl-devel') }
 
-  context "with default param" do
+  context 'with default param' do
 
     it do
       should contain_group('openresty group').with({
@@ -64,6 +64,60 @@ describe 'openresty' do
         'notify'  => 'Exec[install openresty]',
       })
     end
-    
+
+    it do
+      should contain_exec('install openresty').with({
+        'cwd'     => '/tmp/ngx_openresty-1.7.0.1',
+        'path'    => '/sbin:/bin:/usr/bin',
+        'command' => 'make && make install',
+        'creates' => '/usr/local/openresty/nginx/sbin/nginx',
+        'require' => ['User[openresty user]', 'Exec[configure openresty]'],
+      })
+    end
+
+    it do
+      should contain_service('nginx').with({
+        'ensure'     => 'running',
+        'name'       => 'nginx',
+        'enable'     => 'true',
+        'hasrestart' => 'false',
+        'restart'    => '/etc/init.d/nginx reload',
+        'require'    => 'Exec[install openresty]',
+      })
+    end
+  end
+
+  context 'with user group param' do
+    let(:params) { {:user => 'openresty', :group => 'openresty'} }
+
+    it do
+      should contain_group('openresty group').with({
+        'ensure' => 'present',
+        'name'   => 'openresty',
+      })
+    end
+
+    it do
+      should contain_user('openresty user').with({
+        'ensure'  => 'present',
+        'name'    => 'openresty',
+        'groups'  => 'openresty',
+        'comment' => 'nginx web server',
+        'shell'   => '/sbin/nologin',
+        'system'  => 'true',
+        'require' => 'Group[openresty group]',
+      })
+    end
+
+    it do
+      should contain_exec('configure openresty').with({
+        'cwd'     => '/tmp/ngx_openresty-1.7.0.1',
+        'path'    => '/sbin:/bin:/usr/bin',
+        'command' => '/tmp/ngx_openresty-1.7.0.1/configure --user=openresty --group=openresty',
+        'creates' => '/tmp/ngx_openresty-1.7.0.1/build',
+        'require' => ['Package[perl]', 'Package[gcc]', 'Package[readline-devel]', 'Package[pcre-devel]', 'Package[openssl-devel]'],
+        'notify'  => 'Exec[install openresty]',
+      })
+    end
   end
 end
