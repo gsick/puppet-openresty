8cf96e6e7c9811ed77597cab32efe69937cd666b
gsick <gamaliel.sick@yahoo.fr>
2014-06-28 17:06:48 +0200

    initial import

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,14 +36,17 @@
 # Copyright 2014 Agilience, Gamaliel Sick, unless otherwise noted.
 #
 class openresty(
-  $version            = hiera('openresty::version', '1.7.0.1'),
-  $user               = hiera('openresty::user', 'nginx'),
-  $group              = hiera('openresty::group', 'nginx'),
-  $nginx_like_install = hiera('openresty::nginx_like_install', false),
-  $configure_params   = hiera_array('openresty::configure_params', []),
-  $tmp                = hiera('openresty::tmp', '/tmp'),
-  $service_ensure     = hiera('openresty::service_ensure', 'running'),
-  $service_enable     = hiera('openresty::service_enable', 'true'),
+  $version                = hiera('openresty::version', '1.7.0.1'),
+  $user                   = hiera('openresty::user', 'nginx'),
+  $group                  = hiera('openresty::group', 'nginx'),
+  $nginx_like_install     = hiera('openresty::nginx_like_install', false),
+  $configure_params       = hiera_array('openresty::configure_params', []),
+  $with_pcre              = hiera(openresty::with_pcre, false),
+  $with_lua_resty_http    = hiera('openresty::with_lua_resty_http', false),
+  $lua_resty_http_version = hiera('openresty::lua_resty_http_version', '0.03'),
+  $tmp                    = hiera('openresty::tmp', '/tmp'),
+  $service_ensure         = hiera('openresty::service_ensure', 'running'),
+  $service_enable         = hiera('openresty::service_enable', 'true'),
 ) {
 
   validate_string($version)
@@ -51,6 +54,8 @@ class openresty(
   validate_string($group)
   validate_bool($nginx_like_install)
   validate_array($configure_params)
+  validate_bool($with_lua_resty_http)
+  validate_string($lua_resty_http_version)
   validate_absolute_path($tmp)
   validate_string($service_ensure)
 
@@ -165,4 +170,33 @@ class openresty(
     restart    => '/etc/init.d/nginx reload',
     require    => [Exec['install openresty'], File['openresty init script']],
   }
+
+  if($with_lua_resty_http) {
+    exec { 'download lua-resty-http':
+      cwd     => $tmp,
+      path    => '/sbin:/bin:/usr/bin',
+      command => "wget -O lua-resty-http-${lua_resty_http_version}.tar.gz https://github.com/pintsized/lua-resty-http/archive/v${lua_resty_http_version}.tar.gz",
+      creates => "${tmp}/lua-resty-http-${lua_resty_http_version}.tar.gz",
+      notify  => Exec['untar lua-resty-http'],
+      require => Package['wget'],
+    }
+
+    exec { 'untar lua-resty-http':
+      cwd     => $tmp,
+      path    => '/sbin:/bin:/usr/bin',
+      command => "tar -zxvf lua-resty-http-${lua_resty_http_version}.tar.gz",
+      creates => "${tmp}/lua-resty-http-${lua_resty_http_version}/Makefile",
+      notify  => Exec['install lua-resty-http'],
+    }
+
+    exec { 'install lua-resty-http':
+      cwd     => "${tmp}/lua-resty-http-${lua_resty_http_version}",
+      path    => '/sbin:/bin:/usr/bin',
+      command => "cp -f lib/resty/http.lua /usr/local/openresty/lualib/resty",
+      creates => "/usr/local/openresty/lualib/resty/http.lua",
+      require => Exec['install openresty'],
+      notify  => Service['nginx'],
+    }
+  }
+
 }
